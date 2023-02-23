25a0bfa556c28e498b0c917760938e1e30bb7721
Zhafran Dzaky <zhafrandzaky@gmail.com>
2023-02-23 23:40:06 +0700

    update

--- dist/script.js
+++ dist/script.js
@@ -9,3 +9,38 @@ window.onscroll = function () {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// hamburger animation
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const ulNav = navMenu.querySelector("ul");
+const liNav = navMenu.querySelectorAll("li");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+  navMenu.classList.toggle("navbar-active");
+  ulNav.classList.toggle("ul-active");
+  liNav.forEach((e) => {
+    e.classList.toggle("li-active");
+  });
+});
+
+// sidebar
+window.addEventListener("resize", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+  const ulNav = navMenu.querySelector("ul");
+  const liNav = navMenu.querySelectorAll("li");
+  if (
+    window.innerWidth >= 1024 &&
+    hamburger.classList.contains("hamburger-active")
+  ) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+    navMenu.classList.remove("navbar-active");
+    ulNav.classList.remove("ul-active");
+    liNav.forEach((e) => {
+      e.classList.remove("li-active");
+    });
+  }
+});
